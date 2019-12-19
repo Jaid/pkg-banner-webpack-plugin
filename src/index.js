5508c43dd3bebecf97f730fd3306e534940ee5f4
Jaid <jaid.jsx@gmail.com>
2019-12-19 02:22:44 +0100

    Initial implementation
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,199 +1,70 @@
 /** @module pkg-banner-webpack-plugin */
 
 import fsp from "@absolunet/fsp"
-import ensureArray from "ensure-array"
-import ensureObject from "ensure-object"
-import ow from "ow"
-import resolveAny from "resolve-any"
-import xmlWriter from "xmlbuilder"
+import path from "path"
+import {ConcatSource} from "webpack-sources"
 
-import applications from "lib/applications.yml"
+import template from "./template.hbs"
 
-const webpackId = "PkgBannerWebpackPlugin"
+const debug = require("debug")(_PKG_NAME)
 
 /**
  * @typedef {Object} Options
- * @prop {string} fileName=CSXS/manifest.xml
- * @prop {string} identifier
- * @prop {string} version=1.0.0
- * @prop {string} title
- * @prop {string} requiredCefVersion=5.0
- * @prop {Object<"photoshop"|"illustrator"|"indesign"|"incopy"|"premierePro"|"prelude"|"afterEffects"|"animate"|"audition"|"dreamweaver"|"muse"|"bridge"|"rush",string|string[]>} apps={photoshop: "20"}
- * @prop {string} mainPath=./index.html
- * @prop {boolean|PanelOptions} panel=false
- * @prop {boolean} minify=true
- * @prop {boolean} debug=`webpackMode==="development"`
- * @prop {number} debugPort=8400
- * @prop {string} debugFileName=".debug"
- * @prop {string} [scriptSourceFile] Absolute path to a JavaScript file that will be used as the host script entry point
- * @prop {string} scriptFileName=client.jsx
- * @prop {string|string[]} cefParams=["--enable-nodejs","--mixed-context"]
+ * @prop {Object} pkg If given, package.json will not be read and this value will be used instead
  */
 
-/**
- * @typedef {Object} PanelOptions
- * @prop {string} title=this.options.title
- * @prop {number} width=200
- * @prop {number} height=600
- */
-
-const xmlCreateOptions = {encoding: "UTF-8"}
+const webpackId = "PkgBannerPlugin"
 
 /**
  * @class
  */
-export default class {
+export default class PkgBannerPlugin {
 
   /**
    * @constructor
    * @param {Options} [options] Plugin options
    */
   constructor(options) {
-    const optionsObject = ensureObject(options, "identifier")
-    /**
-     * @type {Options}
-     */
     this.options = {
-      fileName: "CSXS/manifest.xml",
-      version: "1.0.0",
-      title: optionsObject.title || optionsObject.identifier,
-      requiredCefVersion: "9.0",
-      apps: {
-        photoshop: "20",
-      },
-      mainPath: "./index.html",
-      panel: true,
-      minify: true,
-      debug: null,
-      debugPort: 8400,
-      debugFileName: ".debug",
-      scriptSourceFile: null,
-      scriptFileName: "client.jsx",
-      cefParams: ["--enable-nodejs", "--mixed-context"],
-      ...optionsObject,
+      ...options,
     }
-    ow(this.options.identifier, ow.string.nonEmpty)
   }
 
   /**
    * @param {import("webpack").Compiler} compiler
    */
   apply(compiler) {
-    compiler.hooks.emit.tapPromise(webpackId, async compilation => {
-      const extensionManifestVersion = "8.0"
-      const extensionId = `${this.options.identifier}.extension`
-      const hosts = []
-      for (const [appId, requiredVersion] of Object.entries(this.options.apps)) {
-        const app = applications[appId]
-        hosts.push({
-          "@Name": app.host,
-          "@Version": requiredVersion,
-        })
-      }
-      const model = {
-        ExtensionManifest: {
-          "@xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
-          "@Version": extensionManifestVersion,
-          "@ExtensionBundleId": this.options.identifier,
-          "@ExtensionBundleVersion": this.options.version,
-          "@ExtensionBundleName": this.options.title,
-          ExtensionList: {
-            Extension: {
-              "@Id": extensionId,
-              "@Version": this.options.version,
-            },
-          },
-          ExecutionEnvironment: {
-            RequiredRuntimeList: {
-              RequiredRuntime: {
-                "@Name": "CSXS",
-                "@Version": this.options.requiredCefVersion,
-              },
-            },
-            HostList: {
-              Host: hosts,
-            },
-            LocaleList: {
-              Locale: {
-                "@Code": "All",
-              },
-            },
-          },
-          DispatchInfoList: {
-            Extension: {
-              "@Id": extensionId,
-              DispatchInfo: {
-                Resources: {
-                  MainPath: this.options.mainPath,
-                },
-                Lifecycle: {
-                  AutoVisible: true,
-                },
-              },
-            },
-          },
-        },
-      }
-      if (this.options.panel) {
-        const panelOptions = {
-          title: this.options.title,
-          height: 600,
-          width: 200,
+    compiler.hooks.compilation.tap(webpackId, compilation => {
+      compilation.hooks.optimizeChunkAssets.tapPromise(webpackId, async chunks => {
+        let pkg = this.options.pkg
+        if (!pkg) {
+          const file = path.join(compiler.context, "package.json")
+          debug(`Reading ${file}`)
+          const fileExists = await fsp.pathExists(file)
+          if (!fileExists) {
+            return
+          }
+          pkg = await fsp.readJson(file)
         }
-        model.ExtensionManifest.DispatchInfoList.Extension.DispatchInfo.UI = {
-          Type: "Panel",
-          Menu: panelOptions.title,
-          Geometry: {
-            Size: {
-              Height: panelOptions.height,
-              Width: panelOptions.width,
-            },
-          },
+        const context = {
+          pkg,
+          options: this.options,
+          title: pkg.title || pkg.domain || pkg.name,
+          license: pkg.license || "MIT",
+          year: (new Date).getFullYear(),
         }
-      }
-      if (this.options.cefParams) {
-        const params = ensureArray(this.options.cefParams)
-        model.ExtensionManifest.DispatchInfoList.Extension.DispatchInfo.Resources.CEFCommandLine = {
-          Parameter: params,
+        if (!context.title) {
+          context.title = path.basename(compiler.context)
         }
-      }
-      if (this.options.scriptSourceFile) {
-        const scriptContent = await fsp.readFile(this.options.scriptSourceFile, "utf8")
-        const scriptFileName = await resolveAny(this.options.scriptFileName)
-        compilation.assets[scriptFileName] = {
-          source: () => scriptContent,
-          size: () => scriptContent.length,
-        }
-        model.ExtensionManifest.DispatchInfoList.Extension.DispatchInfo.Resources.ScriptPath = `./${scriptFileName}`
-      }
-      const content = xmlWriter.create(model, xmlCreateOptions).end({pretty: !this.options.minify})
-      const fileName = await resolveAny(this.options.fileName)
-      compilation.assets[fileName] = {
-        source: () => content,
-        size: () => content.length,
-      }
-      const isDevelopmentMode = compilation.compiler.options.mode === "development"
-      const shouldEmitDebug = this.options.debug === null ? isDevelopmentMode : Boolean(this.options.debug)
-      if (shouldEmitDebug) {
-        let currentDebugPort = this.options.debugPort
-        const debugModel = {
-          ExtensionList: {
-            Extension: {
-              "@Id": extensionId,
-              HostList: {
-                Host: hosts.map(host => {
-                  const debugPort = currentDebugPort
-                  currentDebugPort++
-                  return {
-                    "@Name": host["@Name"],
-                    "@Port": debugPort,
-                  }
-                }),
-              },
-            },
-          },
-        }
+        debug(`Title: ${context.title}`)
+        const banner = template(context)
+        for (const chunk of chunks) {
           for (const chunkFile of chunk.files) {
+            compilation.assets[chunkFile] = new ConcatSource(banner, "\n", compilation.assets[chunkFile])
+          }
+        }
+      })
+    })
   }
 
 }
--- test/test.js
+++ test/test.js
@@ -6,31 +6,34 @@ import pify from "pify"
 import webpack from "webpack"
 
 const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
-const {default: PkgBannerWebpackPlugin} = indexModule
+const {default: PkgBannerPlugin} = indexModule
 
 jest.setTimeout(ms`1 minute`)
 
-const getWepbackConfig = name => ({
+const getWebpackConfig = name => ({
   mode: "production",
   context: path.join(__dirname, name),
   entry: path.join(__dirname, name),
   output: {
     path: path.join(__dirname, "..", "dist", "test", name),
+    filename: "index.js",
   },
 })
 
 it("should run", async () => {
   const name = "basic"
   const webpackConfig = {
-    ...getWepbackConfig(name),
+    ...getWebpackConfig(name),
     plugins: [
       new CleanWebpackPlugin,
-      new PkgBannerWebpackPlugin({
-        identifier: "cep-example",
-        title: "CEP Example",
-      }),
+      new PkgBannerPlugin,
     ],
   }
   await pify(webpack)(webpackConfig)
-  const xml = await fsp.readXml(path.join(__dirname, "..", "dist", "test", name, "CSXS", "manifest.xml"))
+  const outputFile = path.join(webpackConfig.output.path, webpackConfig.output.filename)
+  const exists = await fsp.pathExists(outputFile)
+  expect(exists).toBeTruthy()
+  const output = await fsp.readFile(outputFile, "utf8")
+  const expectedBanner = await fsp.readFile(path.join(__dirname, "basic", "expected.txt"), "utf8")
+  expect(output.startsWith(expectedBanner)).toBeTruthy()
 })
