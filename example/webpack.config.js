6f1e84e8011ca3c0c7d5113c220d3c0a4d131e8c
Jaid <jaid.jsx@gmail.com>
2019-12-21 03:50:23 +0100

    manage: Moved test to folder `example`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

---
+++ example/webpack.config.js
@@ -0,0 +1,23 @@
+const {CleanWebpackPlugin} = require("clean-webpack-plugin")
+const path = require("path")
+
+/**
+ * @function
+ * @param {Object} TestPlugin
+ * @return {import("webpack").Configuration}
+ */
+module.exports = TestPlugin => {
+  return {
+    mode: "production",
+    context: path.join(__dirname, "..", "example"),
+    entry: path.join(__dirname, "..", "example", "src"),
+    output: {
+      path: path.join(__dirname, "..", "example", "dist"),
+      filename: "index.js",
+    },
+    plugins: [
+      new CleanWebpackPlugin,
+      new TestPlugin,
+    ],
+  }
+}
--- test/test.js
+++ test/test.js
@@ -1,5 +1,4 @@
 import fsp from "@absolunet/fsp"
-import {CleanWebpackPlugin} from "clean-webpack-plugin"
 import ms from "ms.macro"
 import path from "path"
 import pify from "pify"
@@ -10,30 +9,14 @@ const {default: PkgBannerPlugin} = indexModule
 
 jest.setTimeout(ms`1 minute`)
 
-const getWebpackConfig = name => ({
-  mode: "production",
-  context: path.join(__dirname, name),
-  entry: path.join(__dirname, name),
-  output: {
-    path: path.join(__dirname, "..", "dist", "test", name),
-    filename: "index.js",
-  },
-})
-
 it("should run", async () => {
-  const name = "basic"
-  const webpackConfig = {
-    ...getWebpackConfig(name),
-    plugins: [
-      new CleanWebpackPlugin,
-      new PkgBannerPlugin,
-    ],
-  }
+  const generateWebpackConfig = require(path.join(__dirname, "..", "example", "webpack.config.js"))
+  const webpackConfig = generateWebpackConfig(PkgBannerPlugin)
   await pify(webpack)(webpackConfig)
   const outputFile = path.join(webpackConfig.output.path, webpackConfig.output.filename)
   const exists = await fsp.pathExists(outputFile)
   expect(exists).toBeTruthy()
   const output = await fsp.readFile(outputFile, "utf8")
-  const expectedBanner = await fsp.readFile(path.join(__dirname, "basic", "expected.txt"), "utf8")
+  const expectedBanner = await fsp.readFile(path.join(__dirname, "expected.txt"), "utf8")
   expect(output.startsWith(expectedBanner)).toBeTruthy()
 })
