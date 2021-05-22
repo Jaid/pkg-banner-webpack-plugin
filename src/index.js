4f5cad4aada4a42051e389f9a1d1e01d92dd1a75
Jaid <jaid.jsx@gmail.com>
2021-05-22 10:03:30 +0200

    improve: Reverted to more efficient precompiled Handlebars template
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
 /** @module pkg-banner-webpack-plugin */
 
 import fsp from "@absolunet/fsp"
-import Handlebars from "handlebars"
 import path from "path"
 import webpack from "webpack"
 import {ConcatSource} from "webpack-sources"
 
-// TODO: handlebars-loader does not work anymore for some reason, so I temporarily installed handlebars as prod dependency
-const template = Handlebars.compile(`/*!
-{{#if pkg.version}}
-*** {{{title}}} {{{pkg.version}}}
-{{else}}
-*** {{{title}}}
-{{/if}}
-{{#if pkg.author}}
-*** Copyright © {{{year}}}, {{{pkg.author}}}
-{{else}}
-*** Copyright © {{{year}}}
-{{/if}}
-*** @license {{{license}}}
-{{#if pkg.homepage}}
-*** See {{{pkg.homepage}}}
-{{/if}}
-!*/`)
+import template from "./template.hbs"
 
 const debug = require("debug")(process.env.REPLACE_PKG_NAME)
 
